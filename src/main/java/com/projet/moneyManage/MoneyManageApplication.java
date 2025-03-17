f30c33df537a91312b7ca9b395d965c75d99e028
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-03-17 09:07:06 +0300

    ok

---
+++ src/main/java/com/projet/moneyManage/MoneyManageApplication.java
@@ -0,0 +1,13 @@
+package com.projet.moneyManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyManageApplication.class, args);
+	}
+
+}
