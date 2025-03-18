60ab5290e41f2e832010b6c8512c2ca280d077dd
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-03-18 14:11:42 +0300

    ok

--- src/main/java/com/projet/moneyManage/controllers/DepenseController.java
+++ src/main/java/com/projet/moneyManage/controllers/DepenseController.java
@@ -1,14 +1,20 @@
 package com.projet.moneyManage.controllers;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import com.projet.moneyManage.models.Depense;
+import com.projet.moneyManage.models.Revenu;
 import com.projet.moneyManage.services.DepenseService;
+import com.projet.moneyManage.services.TypeDepenseService;
+import com.projet.moneyManage.services.UtilisateurService;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -17,6 +23,11 @@ import jakarta.servlet.http.HttpSession;
 public class DepenseController {
     @Autowired
     private DepenseService depenseService;
+    @Autowired
+    private TypeDepenseService typeDepenseService;
+
+    @Autowired
+    private UtilisateurService utilisateurService;
 
     @GetMapping
     public ModelAndView findAllByUtilisateur(HttpSession session ){
@@ -32,5 +43,33 @@ public class DepenseController {
         return modelAndView;
     }
 
-    @GetMapping
+    @GetMapping("/add-form")
+    public ModelAndView addForm(){
+        ModelAndView modelAndView=new ModelAndView("template");
+        modelAndView.addObject("page", "depenses/ajout");
+        modelAndView.addObject("types", typeDepenseService.findAll());
+        return modelAndView;
+    }
+
+
+    @PostMapping("/add")
+    public ModelAndView add(@RequestParam Double montant,@RequestParam LocalDateTime dateDepense,@RequestParam String raison,@RequestParam Long idTypeDepense,HttpSession session){
+        ModelAndView modelAndView=new ModelAndView("template");
+        try {
+            Long idUtilisateur=(Long) session.getAttribute("utilisateurId");
+            Depense depense=new Depense();
+            depense.setMontant(montant);
+            depense.setRaison(raison);
+            depense.setDateDepense(dateDepense);
+            depense.setTypeDepense(typeDepenseService.findById(idTypeDepense));
+            depense.setUtilisateur(utilisateurService.findById(idUtilisateur));
+            depenseService.saveDepense(depense);
+            modelAndView.addObject("succes","Depense ajouté avec succes");
+        } catch (Exception e) {
+            modelAndView.addObject("erreur",e.getMessage());
+        }
+        modelAndView.addObject("page","depenses/ajout");
+        modelAndView.addObject("types", typeDepenseService.findAll());
+        return modelAndView;
+    }
 }
